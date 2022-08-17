5c1d2abc01d2cc6266a74717296e6846baef1c40
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2022-08-17 16:48:28 +0200

    chronos-push-notification-driver: Initialize HTTP client
    This HTTP client will be used later to send the formatted payload to the
    push-notification API endpoint.

--- src/chronos-push-notification-driver.c
+++ src/chronos-push-notification-driver.c
@@ -1,12 +1,65 @@
 /* Copyright (c) 2022 Open-Xchange Software GmbH, see the included COPYING file */
 
 #include "lib.h"
+#include "http-client.h"
+#include "iostream-ssl.h"
 #include "push-notification-drivers.h"
 #include "push-notification-plugin.h"
 
+struct chronos_push_notification_driver_global {
+	struct http_client *http_client;
+	int refcount;
+};
+
+static struct chronos_push_notification_driver_global *chronos_global = NULL;
+
 struct chronos_push_notification_driver_config {
 	struct event *event;
+	unsigned int http_max_retries;
+	unsigned int http_timeout_msecs;
 };
+
+static void
+chronos_push_notification_driver_init_global(
+	struct chronos_push_notification_driver_config *config,
+	struct mail_user *user)
+{
+	chronos_global = i_new(struct chronos_push_notification_driver_global, 1);
+
+	struct http_client_settings http_set;
+	i_zero(&http_set);
+	/* This is going to use the first user's settings, but these
+	   are unlikely to change between users so it shouldn't matter
+	   much. */
+	http_set.debug = user->mail_debug;
+	http_set.max_attempts = config->http_max_retries + 1;
+	http_set.request_timeout_msecs = config->http_timeout_msecs;
+	http_set.event_parent = user->event;
+
+	struct ssl_iostream_settings ssl_set;
+	mail_user_init_ssl_client_settings(user, &ssl_set);
+	http_set.ssl = &ssl_set;
+
+	chronos_global->http_client = http_client_init(&http_set);
+	chronos_global->refcount = 1;
+}
+
+static void
+chronos_push_notification_driver_global_unref(void)
+{
+	if (chronos_global != NULL) {
+		if (chronos_global->http_client != NULL)
+			http_client_wait(chronos_global->http_client);
+		i_assert(chronos_global->refcount > 0);
+		--chronos_global->refcount;
+
+		if (chronos_global->refcount <= 0) {
+			if (chronos_global->http_client != NULL)
+				http_client_deinit(&chronos_global->http_client);
+			i_free(chronos_global);
+		}
+	}
+}
 
 static int
 chronos_push_notification_driver_init(
@@ -20,6 +73,13 @@ chronos_push_notification_driver_init(
 	dconfig->event = event_create(user->event);
 	event_add_category(dconfig->event, push_notification_get_event_category());
 	event_set_append_log_prefix(dconfig->event, "push-notification-chronos: ");
+
+	if (chronos_global == NULL) {
+		chronos_push_notification_driver_init_global(dconfig, user);
+	} else {
+		i_assert(chronos_global->refcount > 0);
+		++chronos_global->refcount;
+	}
 
 	*context = dconfig;
 
@@ -47,12 +107,14 @@ chronos_push_notification_driver_deinit(
 {
 	struct chronos_push_notification_driver_config *dconfig = duser->context;
 	event_unref(&dconfig->event);
+
+	chronos_push_notification_driver_global_unref();
 }
 
 static void
 chronos_push_notification_driver_cleanup(void)
 {
-	return;
+	chronos_push_notification_driver_global_unref();
 }
 
 /* Driver definition */
