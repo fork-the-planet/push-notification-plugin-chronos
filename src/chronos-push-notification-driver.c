cfc37308c25b91ae058f59a1f2f8662c90209a96
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2022-08-18 16:59:28 +0200

    chronos-push-notification-driver: Implement message-processing
    Note: Currently the transaction is not correctly enabled and thus no
          message will ever be processed.

--- src/chronos-push-notification-driver.c
+++ src/chronos-push-notification-driver.c
@@ -4,9 +4,17 @@
 #include "hash.h"
 #include "http-client.h"
 #include "http-url.h"
+#include "imap-bodystructure.h"
 #include "iostream-ssl.h"
+#include "istream.h"
+#include "json-parser.h"
+#include "mail-storage-private.h"
+#include "message-part-data.h"
+#include "message-size.h"
 #include "push-notification-drivers.h"
+#include "push-notification-event-messagenew.h"
 #include "push-notification-plugin.h"
+#include "push-notification-txn-msg.h"
 #include "settings-parser.h"
 #include "str.h"
 
@@ -15,6 +23,11 @@
 #define DEFAULT_RETRY_COUNT 1
 #define DEFAULT_TIMEOUT_MSECS 2000
 
+/* Calendar invite search parameters. */
+#define CHRONOS_PN_ICAL_MIME_TYPE "text"
+#define CHRONOS_PN_ICAL_MIME_SUBTYPE "calendar"
+#define CHRONOS_PN_ICAL_ATC_SUFFIX "ics"
+
 struct chronos_push_notification_driver_global {
 	struct http_client *http_client;
 	int refcount;
@@ -22,7 +35,15 @@ struct chronos_push_notification_driver_global {
 
 static struct chronos_push_notification_driver_global *chronos_global = NULL;
 
+struct chronos_push_notification_driver_http_ctx {
+	pool_t pool;
+	string_t *json;
+	struct event *event;
+	struct istream *payload;
+};
+
 struct chronos_push_notification_driver_config {
+	struct chronos_push_notification_driver_http_ctx *http_ctx;
 	struct event *event;
 	struct http_url *http_url;
 	unsigned int http_max_retries;
@@ -168,12 +189,276 @@ chronos_push_notification_driver_begin_txn(
 	return FALSE;
 }
 
+static bool
+chronos_push_notification_driver_ical_search_part(
+	const struct message_part_data *data)
+{
+	/* Check #1: Look for text/calendar parts. */
+	if (data->content_type != NULL && data->content_subtype != NULL &&
+	    (strcasecmp(data->content_type, CHRONOS_PN_ICAL_MIME_TYPE) == 0) &&
+	    (strcasecmp(data->content_subtype, CHRONOS_PN_ICAL_MIME_SUBTYPE) == 0))
+		return TRUE;
+
+	/* Check #2: Look for ".ics" suffix in attachment filenames. */
+	const char *ext;
+	for (unsigned int i = 0; i < data->content_disposition_params_count; i++) {
+		const struct message_part_param *param =
+			&data->content_disposition_params[i];
+
+		if (param->name != NULL &&
+		    strcasecmp(param->name, "filename") == 0 &&
+		    param->value != NULL) {
+			if (((ext = strrchr(param->value, '.')) != NULL) &&
+			    (strcasecmp(ext + 1, CHRONOS_PN_ICAL_ATC_SUFFIX) == 0))
+				return TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
+static bool
+chronos_push_notification_driver_ical_search_allparts(
+	const struct message_part *initial_part)
+{
+	ARRAY(struct message_part) parts;
+	t_array_init(&parts, 1);
+	array_push_back(&parts, initial_part);
+
+	while (array_count(&parts) > 0) {
+		const struct message_part *part = array_front(&parts);
+
+		if (part->data != NULL &&
+		    chronos_push_notification_driver_ical_search_part(part->data))
+			return TRUE;
+		if (part->next != NULL)
+			array_push_back(&parts, part->next);
+		if (part->children != NULL)
+			array_push_back(&parts, part->children);
+
+		array_pop_front(&parts);
+	}
+	return FALSE;
+}
+
+static bool
+chronos_push_notification_driver_ical_search(
+	struct chronos_push_notification_driver_config *dconfig,
+	struct mail *mail)
+{
+	struct message_part *all_parts;
+	if (mail_get_parts(mail, &all_parts) < 0)
+		return FALSE;
+
+	const char *bodystructure;
+	if ((all_parts->data == NULL) &&
+	    (mail_get_special(mail, MAIL_FETCH_IMAP_BODYSTRUCTURE,
+			      &bodystructure) < 0))
+		return FALSE;
+
+	const char *error;
+	pool_t bodystructure_parse_pool = pool_alloconly_create(
+		"chronos bodystructure parse", 512);
+	if ((all_parts->data == NULL) &&
+	    (imap_bodystructure_parse(bodystructure, bodystructure_parse_pool,
+				      all_parts, &error) < 0)) {
+		e_error(dconfig->event, "%s", error);
+		return FALSE;
+	}
+
+	/* Parts now have bodystructure information populated. This info is
+	 * contained in each part's context struct member. */
+	bool has_ical = chronos_push_notification_driver_ical_search_allparts(all_parts);
+	pool_unref(&bodystructure_parse_pool);
+	return has_ical;
+}
+
+static void
+chronos_push_notification_driver_handle_mail_error(
+	struct chronos_push_notification_driver_config *dconfig,
+	struct mail *mail, uint32_t mail_uid, const char *prefix)
+{
+	enum mail_error error;
+	const char *errstr = mailbox_get_last_internal_error(mail->box, &error);
+
+	if (error == MAIL_ERROR_EXPUNGED)
+		e_info(dconfig->event, "%s %u: %s", prefix, mail_uid, errstr);
+	else
+		e_error(dconfig->event, "%s %u: %s", prefix, mail_uid, errstr);
+}
+
+static bool
+chronos_push_notification_driver_read_mail_body(
+	struct mailbox_transaction_context *t,
+	struct chronos_push_notification_driver_config *dconfig,
+	uint32_t mail_uid, string_t **body)
+{
+	struct mail *mail = mail_alloc(t, MAIL_FETCH_STREAM_BODY, NULL);
+	if (!mail_set_uid(mail, mail_uid)) {
+		chronos_push_notification_driver_handle_mail_error(
+			dconfig, mail, mail_uid,
+			"Unable to fetch email with uid");
+		mail_free(&mail);
+		return FALSE;
+	}
+
+	/* Make sure email contains relevant calendar data. */
+	if (!chronos_push_notification_driver_ical_search(dconfig, mail)) {
+		e_debug(dconfig->event, "Mail does not contain calendar invite");
+		mail_free(&mail);
+		return FALSE;
+	}
+
+	/* get body via stream */
+	struct istream *input;
+	struct message_size hdr_size, body_size;
+	if (mail_get_stream(mail, &hdr_size, &body_size, &input) < 0) {
+		chronos_push_notification_driver_handle_mail_error(
+			dconfig, mail, mail_uid,
+			"Unable to get mail stream for uid");
+		mail_free(&mail);
+		return FALSE;
+	}
+
+	/* Check message body size against configured max size. */
+	uoff_t full_size = hdr_size.physical_size + body_size.physical_size;
+	if (full_size > dconfig->msg_max_size) {
+		e_debug(dconfig->event,
+			"Mail exceeds configured max size. "
+			"Mail: %"PRIuUOFF_T"B vs. Max Size: %"PRIuUOFF_T"B",
+			full_size, dconfig->msg_max_size);
+		mail_free(&mail);
+		return FALSE;
+	}
+
+	/* append data from stream to body str_t */
+	const unsigned char *data;
+	size_t size;
+	ssize_t ret;
+	while ((ret = i_stream_read_more(input, &data, &size)) > 0) {
+		size_t body_len = str_len(*body);
+		if (body_len < dconfig->msg_max_size) {
+			size_t max_size = I_MIN(size, dconfig->msg_max_size - body_len);
+			str_append_data(*body, data, max_size);
+			i_stream_skip(input, max_size);
+		} else
+			break;
+	}
+	/* This assertion might trigger if there was more data read, than
+	   initially anticipated. */
+	i_assert(ret == -1);
+
+	if (input->stream_errno != 0) {
+		e_error(dconfig->event, "read(%s) failed: %s",
+			i_stream_get_name(input),
+			i_stream_get_error(input));
+		mail_free(&mail);
+		return FALSE;
+	}
+
+	mail_free(&mail);
+	return TRUE;
+}
+
+static void
+chronos_push_notification_driver_http_callback(
+	const struct http_response *response,
+	struct chronos_push_notification_driver_http_ctx *ctx)
+{
+	switch (response->status / 100) {
+	case 2:
+		/* Success */
+		e_debug(ctx->event, "Notification sent successfully: %s",
+			http_response_get_message(response));
+		break;
+
+	default:
+		/* Error */
+		e_error(ctx->event, "Error when sending notification: %s",
+			http_response_get_message(response));
+		break;
+	}
+
+	/* Cleanup */
+	i_stream_unref(&ctx->payload);
+	str_free(&ctx->json);
+	event_unref(&ctx->event);
+	pool_unref(&ctx->pool);
+}
+
 static void
 chronos_push_notification_driver_process_msg(
-	struct push_notification_driver_txn *dtxn ATTR_UNUSED,
-	struct push_notification_txn_msg *msg ATTR_UNUSED)
+	struct push_notification_driver_txn *dtxn,
+	struct push_notification_txn_msg *msg)
 {
-	return;
+	struct chronos_push_notification_driver_config *dconfig =
+		dtxn->duser->context;
+
+	/* Only push message on "MessageNew" events. */
+	struct push_notification_event_messagenew_data *message_new =
+		push_notification_txn_msg_get_eventdata(msg, "MessageNew");
+	if (message_new == NULL)
+		return;
+
+	/* Open and sync new instance of the mailbox to get most recent
+	   status/data. */
+	struct mailbox *mbox = mailbox_alloc(
+			mailbox_get_namespace(dtxn->ptxn->mbox)->list,
+			mailbox_get_vname(dtxn->ptxn->mbox), MAILBOX_FLAG_READONLY);
+	if (mailbox_sync(mbox, 0) < 0) {
+		e_error(dconfig->event, "mailbox_sync(%s) failed: %s",
+			mailbox_get_vname(dtxn->ptxn->mbox),
+			mailbox_get_last_internal_error(mbox, NULL));
+	}
+
+	pool_t http_ctx_pool = pool_alloconly_create("chronos http context", 512);
+	struct mailbox_transaction_context *t = mailbox_transaction_begin(
+		mbox, 0, __func__);
+	string_t *body = str_new(http_ctx_pool, dconfig->msg_max_size);
+	if (!chronos_push_notification_driver_read_mail_body(t, dconfig,
+							     msg->uid, &body)) {
+		(void)mailbox_transaction_commit(&t);
+		mailbox_free(&mbox);
+		pool_unref(&http_ctx_pool);
+		return;
+	}
+
+	(void)mailbox_transaction_commit(&t);
+	mailbox_free(&mbox);
+
+	/* Setup context for http request. This is done to split http resources
+	   from the main handling of the push notification message resources,
+	   that are done in a synchronous manner. */
+	struct chronos_push_notification_driver_http_ctx *ctx = p_new(
+		http_ctx_pool, struct chronos_push_notification_driver_http_ctx, 1);
+	ctx->pool = http_ctx_pool;
+	ctx->event = event_create(dconfig->event);
+	event_add_category(ctx->event, push_notification_get_event_category());
+	event_set_append_log_prefix(ctx->event, "http callback: ");
+
+	/* Setup http request. */
+	struct http_client_request *http_req = http_client_request_url(
+		chronos_global->http_client, "PUT", dconfig->http_url,
+		chronos_push_notification_driver_http_callback, ctx);
+	http_client_request_set_event(http_req, dtxn->ptxn->event);
+	http_client_request_add_header(http_req, "Content-Type",
+				       "application/json; charset=utf-8");
+
+	/* Create json payload to send. */
+	ctx->json = str_new(ctx->pool, dconfig->msg_max_size);
+	str_append(ctx->json, "{\"user\":\"");
+	json_append_escaped(ctx->json, dtxn->ptxn->muser->username);
+	str_append(ctx->json, "\",\"event\":\"messageNew\",\"folder\":\"");
+	json_append_escaped(ctx->json, msg->mailbox);
+	str_append(ctx->json, "\",\"body\":\"");
+	json_append_escaped(ctx->json, str_c(body));
+	str_append(ctx->json, "\"}");
+
+	/* Set payload to http request and press send. */
+	ctx->payload = i_stream_create_from_data(str_data(ctx->json),
+						 str_len(ctx->json));
+	http_client_request_set_payload(http_req, ctx->payload, FALSE);
+	http_client_request_submit(http_req);
 }
 
 static void
