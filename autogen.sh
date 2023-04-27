f76b7894b585f998e095edc41fed80acc1d7541c
Aki Tuomi <aki.tuomi@open-xchange.com>
2023-04-27 23:26:54 +0300

    autogen: Always overwrite old files

--- autogen.sh
+++ autogen.sh
@@ -3,15 +3,11 @@
 # If you've non-standard directories, set these
 #GETTEXT_DIR=
 
-if ! test -f build-aux/config.rpath; then
-  for dir in $GETTEXT_DIR /usr/share/gettext /usr/local/share/gettext; do
-    if test -f $dir/config.rpath; then
-      /bin/cp -f $dir/config.rpath build-aux/
-      break
-    fi
-  done
-fi
+for dir in $GETTEXT_DIR /usr/share/gettext; do
+  if test -f $dir/config.rpath; then
+    /bin/cp -f $dir/config.rpath build-aux/
+    break
+  fi
+done
 
-autoreconf -i
-
-rm -f ChangeLog
+autoreconf -vif
